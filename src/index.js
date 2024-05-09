9d181e68cb03a926426c066bf91dbd7850fffb04
nedelnik <p.nedelnik@email.cz>
2024-05-10 00:15:36 +0200

    YES

--- src/index.js
+++ src/index.js
@@ -1,91 +1,107 @@
-d3.csv("data/kc_demo.csv").then(function(data){
-    // Specify the chart’s dimensions.
-    const width = 928;
+d3.csv('data/tmp.csv').then(function(data){
+    const width = 920;
     const height = 600;
-
-    // Compute the graph and start the force simulation.
+    
     const root = d3.stratify()
         .id((d) => d.id)
         .parentId((d) => d.parent)
     (data);
-    const links = root.links();
-    const nodes = root.descendants();
 
-    const simulation = d3.forceSimulation(nodes)
-        // attraction of connected nodes
-        .force("link", d3.forceLink(links).id(d => d.id).distance(0).strength(1))
-        // repulsion of all nodes
-        .force("charge", d3.forceManyBody().strength(-50))
-        // towards center
-        .force("x", d3.forceX())
-        .force("y", d3.forceY());
+    let links, nodes; // calculated in update
+
 
     // Create the container SVG.
-    const svg = d3.create("svg")
+    const svg = d3.select('svg')
         .attr("width", width)
         .attr("height", height)
         .attr("viewBox", [-width / 2, -height / 2, width, height])
         .attr("style", "max-width: 100%; height: auto;")
-        .call(d3.zoom().on("zoom", zoomed));
+        .call(d3.zoom().on("zoom", event => svg.attr("transform", event.transform)));
 
-    // Append links.
-    const link = svg.append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
-    .selectAll("line")
-    .data(links)
-    .join("line");
-
-    // Append nodes.
-    const node = svg.append("g")
-        .attr("fill", "#fff")
-        .attr("stroke", "#000")
-        .attr("stroke-width", 1.5)
-    .selectAll("circle")
-    .data(nodes)
-    .join("circle")
-        .attr("fill", d => d.children ? null : "#000")
-        .attr("stroke", d => d.children ? null : "#fff")
-        .attr("r", 3.5)
-        .call(drag(simulation));
+    const simulation = d3.forceSimulation()
+    // attraction of connected nodes
+    .force("link", d3.forceLink().id(d => d.id).distance(0).strength(d => d.target.data.level / 5))
+    // repulsion of all nodes
+    .force("charge", d3.forceManyBody().strength(-30))
+    // push towards center
+    .force("x", d3.forceX().strength(0.2))
+    .force("y", d3.forceY().strength(0.2))
 
 
-    // Append node labels.
-    const label = svg.append("g")
-    .selectAll("text")
-    .data(nodes.filter(d => !d.children))
-    .join("text")
-    .attr("dy", "0.31em")
-    .attr("x", 7)
-    .text(d => d.data.name)
-    .call(drag(simulation));
-    node.append("title")
-        .text(d => d.data.name);
+    const gLink = svg.append("g")
 
-    simulation.on("tick", () => {
-    link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
+    const gNode = svg.append("g")
 
-    node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
+
+    function update(){
+        links = root.links();
+        nodes = root.descendants();
+    
+        simulation.nodes(nodes);
+        simulation.force("link").links(links);
+
+
+        // Append links.
+        const link = gLink.selectAll("g")
+            .data(links)
+            .join("line")
+                .attr("class", "link");
+
+        // Append nodes.
+        const node = gNode.selectAll("circle")
+            .data(nodes, d => d.id)
+            .join(
+                enter => enter.append("circle")
+                        .attr("fill", 'black')
+                        .attr("stroke", 'black')
+                        .attr("r", 3.5)
+                    .call(drag(simulation))
+                    .on("contextmenu", toggleNode),
+                update => update
+                    .attr("fill", 'red'),
+                exit => exit
+                    .attr("fill", 'blue')
+                    .attr('stroke-opacity', 0)
+                    .remove()
+            )
+
+        console.log(node);
+
+        function toggleNode(event, d) {
+            event.preventDefault();
+
+            var clicked = root.find(node => node.id == d.id);
+
+            clicked._children = clicked.children;
+            clicked.children = null;
+
+            update();
+        }
+
+        simulation.on("tick", () => {
+            link
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
         
-    label
-        .attr("x", d => d.x + 7)
-        .attr("y", d => d.y);
-    });
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+        });
 
-    function zoomed(event) {
-        svg.attr("transform", event.transform);
+        simulation.alpha(1).restart();
     }
 
-    document.getElementById("tree-container").appendChild(svg.node());
+    update();
+
 })
 
-drag = simulation => {
+
+/*
+Run simulation while user is dragging the nodes.
+*/
+function drag(simulation) {
   
     function dragstarted(event, d) {
       if (!event.active) simulation.alphaTarget(0.3).restart();
@@ -108,4 +124,16 @@ drag = simulation => {
         .on("start", dragstarted)
         .on("drag", dragged)
         .on("end", dragended);
+}
+
+function flatten(root) {
+    const nodes = []
+    function recurse(node) {
+      if (node.children) node.children.forEach(recurse)
+      if (!node.id) node.id = ++i;
+      else ++i;
+      nodes.push(node)
+    }
+    recurse(root)
+    return nodes
   }
